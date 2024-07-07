8851f91d58137544c7c8890806293f19a5dc60af
Farid1603 <faridulhaqs16@gmail.com>
2024-07-07 13:34:10 +0530

    Profile pic changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/FaridProfilepic.jpg";
+import profilePic from "../assets/Photo.jpg";
 import {motion} from "framer-motion";
 
 const container =(delay)=>({
